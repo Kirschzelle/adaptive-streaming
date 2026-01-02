9da1215db728cca2385096c822182540db04b0ed
deorhan <deorhan@edu.aau.at>
2026-01-02 11:05:54 +0100

    small fixes (ffmpeg not working atm)

--- streaming/apps.py
+++ streaming/apps.py
@@ -7,23 +7,23 @@ class StreamingConfig(AppConfig):
     def ready(self):
         import streaming.signals
 
-        requeue_interrupted_encodes()
+        #requeue_interrupted_encodes()
 
-def requeue_interrupted_encodes():
-    from django.db.utils import OperationalError, ProgrammingError
-    from .models import CurrentEncode
-    from .tasks import encode_video_resolution
+#def requeue_interrupted_encodes():
+#    from django.db.utils import OperationalError, ProgrammingError
+#    from .models import CurrentEncode
+#    from .tasks import encode_video_resolution
 
-    try:
-        interrupted = CurrentEncode.objects.all()
+#    try:
+#        interrupted = CurrentEncode.objects.all()
 
-        for encode in interrupted:
-            if encode.video_variant:
-                encode_video_resolution.delay(
-                    encode.video_variant.video.id,
-                    encode.video_variant.resolution
-                )
-            encode.delete()
-    except (OperationalError, ProgrammingError):
+#        for encode in interrupted:
+#            if encode.video_variant:
+#                encode_video_resolution.delay(
+#                    encode.video_variant.video.id,
+#                    encode.video_variant.resolution
+#                )
+#            encode.delete()
+#    except (OperationalError, ProgrammingError):
         # Tables don't exist yet (e.g., before migrations run)
-        pass
+#        pass
