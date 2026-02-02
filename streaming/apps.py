c77e0850de4fbc22c6c83549d8f214817cd6fb4a
Kirschzelle <nikolassc@edu.aau.at>
2026-02-02 12:49:52 +0100

    fixed crashing on fresh install

--- streaming/apps.py
+++ streaming/apps.py
@@ -12,15 +12,18 @@ class StreamingConfig(AppConfig):
             requeue_interrupted_encodes()
 
 def requeue_interrupted_encodes():
-    from .models import Video
-    from .tasks import encode_video
-    videos_to_encode = Video.objects.filter(
-        video__isnull=False,
-        processing=False,
-        dash_ready=False
-    ).exclude(video='')
-    
-    count = 0
-    for video in videos_to_encode:
-        encode_video.apply_async(args=[video.id], queue="video_encoding")
-        count += 1
+    try:
+        from .models import Video
+        from .tasks import encode_video
+        videos_to_encode = Video.objects.filter(
+            video__isnull=False,
+            processing=False,
+            dash_ready=False
+        ).exclude(video='')
+
+        count = 0
+        for video in videos_to_encode:
+            encode_video.apply_async(args=[video.id], queue="video_encoding")
+            count += 1
+    except Exception:
+        pass # Note: This can fail when starting the server for the first time since this function gets called when migrations are not done yet, therefore we can just skip it if it fails. On actual server startup migrations will be done and this won't fail.
